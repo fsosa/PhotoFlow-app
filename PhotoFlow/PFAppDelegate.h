c61150232b90be2f1921c77e636fcee36f834e5a
Fidel Sosa <fidel@fidelsosa.com>
2013-11-21 00:04:14 -0500

    Initial Commit

---
+++ PhotoFlow/PFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PFAppDelegate.h
+//  PhotoFlow
+//
+//  Created by Fidel Sosa on 11/21/13.
+//  Copyright (c) 2013 FTJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
